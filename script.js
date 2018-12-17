4dd79d8a8a2a1383181aafb0f573a278d9a4bda1
Nuno <9170511@esmad.ipp.pt>
2018-12-17 10:53:55 +0000

    fortuna

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function calculateDogAge(idade){
 calculateDogAge(2);
 calculateDogAge(5);
 calculateDogAge(8);
+
+function tellFortune(filhos, parceiro, loc, prof){
+    console.log("Voce sera um " + prof + " em " + loc + " e casado com " + parceiro + " com " + filhos + " criancas");
+}
+
+tellFortune("filho1", "parceiro1", "loc1", "prof1");
+tellFortune("filho2", "parceiro2", "loc2", "prof2");
+tellFortune("filho3", "parceiro3", "loc3", "prof3");
