d57503c96975224f8dff90c9ada9371f9a03d3bf
Nuno <9170511@esmad.ipp.pt>
2018-12-17 10:32:02 +0000

    anos cao

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculateDogAge(idade){
+    anos = idade * 7;
+    console.log("O seu cao e " + anos + " anos de idade em anos de cao!");
+}
+
+calculateDogAge(2);
+calculateDogAge(5);
+calculateDogAge(8);
