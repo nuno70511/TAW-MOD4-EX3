5f6517318dd80fa364b25d47b034c80c4cc50391
Nuno <9170511@esmad.ipp.pt>
2018-12-17 10:57:20 +0000

    celsius para fahrenheit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* 01. */
+
 function calculateDogAge(idade){
     anos = idade * 7;
     console.log("O seu cao e " + anos + " anos de idade em anos de cao!");
@@ -7,6 +9,8 @@ calculateDogAge(2);
 calculateDogAge(5);
 calculateDogAge(8);
 
+/* 02. */
+
 function tellFortune(filhos, parceiro, loc, prof){
     console.log("Voce sera um " + prof + " em " + loc + " e casado com " + parceiro + " com " + filhos + " criancas");
 }
@@ -14,3 +18,12 @@ function tellFortune(filhos, parceiro, loc, prof){
 tellFortune("filho1", "parceiro1", "loc1", "prof1");
 tellFortune("filho2", "parceiro2", "loc2", "prof2");
 tellFortune("filho3", "parceiro3", "loc3", "prof3");
+
+/* 03. */
+
+var c = 30;
+var f = function celsiusToFahrenheit(c){
+    return (c * (9/5) + 32);
+}
+
+console.log (c + "°C é " + f(c) + "°F");
